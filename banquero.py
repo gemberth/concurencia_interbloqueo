896003c2cb33412d73ecde47c9d6d3cf174235dd
gemberth delgado <gemberhdelgado@gmail.com>
2022-05-20 00:51:18 -0500

    d

---
+++ banquero.py
@@ -0,0 +1,68 @@
+
+# cliente = hilos o procesos
+# dinero = recursos instancias
+# Banquero = SO con sus recursos 
+# requeridos = maximos
+# asigndos SO  = asignados
+# Instancia o recursos disponibles por SO = disponibles
+maximos = [
+    [3,2,2],
+    [6,1,3],
+    [3,1,4],
+    [4,2,2]
+    ]
+asignados = [
+    [1,0,0],
+    [6,1,2],
+    [2,1,1],
+    [0,0,2]
+    ]
+disponibles = [0,1,1]
+# maximos = [
+#     [1,2,2,1,2],
+#     [2,2,2,1,0],
+#     [2,1,3,1,0],
+#     [1,1,2,2,1]
+#     ]
+# asignados = [
+#     [1,0,2,1,1],
+#     [2,0,1,1,0],
+#     [1,1,0,1,0],
+#     [1,1,1,1,0]
+#     ]
+# disponibles = [0,0,2,1,1]
+
+def comprueba_configuracion(asignados, maximos, disponibles):
+    finalizados = []
+    i = 0
+    while(i < len(asignados)):
+        if not i in finalizados and puede_progresar(asignados[i], disponibles, maximos[i]):
+            print('Finaliza P%s\nDisponibles: %s' % (i,disponibles))
+            libera_recursos(asignados[i], disponibles)
+            finalizados.append(i)   # Marca el proceso Pi como finalizado
+            i = 0
+        else:
+            i += 1
+
+    if(len(asignados) == len(finalizados)):     # Si todos los procesos finalizan
+        print('\nEstado seguro para la configuracion de procesos-recursos dada')
+    else:
+        print('\nSe produce un interbloqueo')
+
+# Incrementamos la lista de disponibles con los que tenia asignados
+def libera_recursos(asignados, disponibles):
+    for i in range(len(disponibles)):
+        disponibles[i] += asignados[i]
+
+# Devuelve True si el nº de elementos asignados mas los disponibles son mayores o iguales a los requeridos para continuar
+def puede_progresar(asignados, disponibles, maximos):
+    resultado = True
+    for i in range(len(maximos)):
+        if(asignados[i] + disponibles[i] < maximos[i]):
+            resultado = False
+            break
+    return resultado
+
+
+
+comprueba_configuracion(asignados, maximos, disponibles)
--- java_dev/src/App.java
+++ java_dev/src/App.java
@@ -1,5 +1,30 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+// Java program to illustrate ConcurrentCollection uses
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.*;
+class ConcurrentDemo extends Thread {
+    static CopyOnWriteArrayList l =   new CopyOnWriteArrayList();
+    
+    
+    public static void main(String[] args)
+        throws InterruptedException
+    {
+        l.add("A");
+        l.add("B");
+        l.add("c");
+  
+        // We create a child thread that is
+        // going to modify ArrayList l.
+        ConcurrentDemo t = new ConcurrentDemo();
+        t.start();
+  
+        // Now we iterate through the ArrayList
+        // and get exception.
+        Iterator itr = l.iterator();
+        while (itr.hasNext()) {
+            String s = (String)itr.next();
+            System.out.println(s);
+            Thread.sleep(6000);
+        }
+        System.out.println(l);
     }
 }
